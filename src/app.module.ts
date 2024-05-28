da1ad31060d47cf4618122a26198acaf308d25b6
gubee <gubee@MacBook-Air-de-gubee.local>
2024-05-27 23:38:34 -0600

    se instala y configura ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mariadb',
+          host: configService.get<string>('DB_HOST'),
+          database: configService.get<string>('DB'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          port: parseInt(configService.get<string>('DB_PORT')),
+          synchronize: configService.get<string>('STATE') !== 'produccion',
+          entities: [],
+        };
+      },
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
